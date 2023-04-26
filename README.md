8feca7bae6661a8b9653221c26cefed8fe5390d5
Bruno <brunorcnoberto@hotmail.com>
2023-04-26 12:38:21 +0100

    adicionado funcionalidade de login

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Teste-EBAC-UI
+
+Ferramentas:
+windows 10
+cypress
+visual studio
+git
+github
+nodejs
+
+usuario: aluno_ebac@teste.com
+senha: teste@teste.com
---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidade login', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir email ou senha invalidos', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('12345')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+
+})
