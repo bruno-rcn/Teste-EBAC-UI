9e04bffb7a761aecfbac5a02265b3307e7c5bfb0
Bruno <brunorcnoberto@hotmail.com>
2023-05-02 08:43:57 +0100

    edicao do cenario de pre cadastro

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -8,12 +8,16 @@ describe('Funcionalidade de Pré cadastro', () => {
     });
     
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        let emailFaker = faker.internet.email()
+        let firstNameFaker = faker.name.firstName()
+        let lastNameFaker = faker.name.lastName()
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('!teste$te#')
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(firstNameFaker)
+        cy.get('#account_last_name').type(lastNameFaker)
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
